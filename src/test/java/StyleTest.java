2c90ada374f2ee0d6bfc14238c37c20992a7f72f
Stewart ilondanga <stilondanga@gmail.com>
2017-12-15 14:15:16 +0300

    add src/test/java/StyleTest.java

--- src/test/java/StyleTest.java
+++ src/test/java/StyleTest.java
@@ -4,3 +4,10 @@ import java.util.ArrayList;
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+
+public class StylistTest {
+  Stylist stylist;
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+  
+}
