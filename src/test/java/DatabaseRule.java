11b5fb1d21c7e5312040a2a1c53d6a82c2d8bf63
Stewart ilondanga <stilondanga@gmail.com>
2017-12-15 15:28:47 +0300

    add src/test/java/DatabaseRule.java

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -8,5 +8,13 @@ public class DatabaseRule extends ExternalResource {
     DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
   }
 
-  
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteStylistsQuery = "DELETE FROM stylists *;";
+      String deleteClientsQuery = "DELETE FROM clients *;";
+      con.createQuery(deleteStylistsQuery).executeUpdate();
+      con.createQuery(deleteClientsQuery).executeUpdate();
+    }
+  }
 }
