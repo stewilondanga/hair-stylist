c947fd257a96a1fbc1a140080512ba42d5bb1fbf
Stewart ilondanga <stilondanga@gmail.com>
2017-12-15 15:33:20 +0300

    add src/test/java/ClientTest.java

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,3 +1,11 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+
+public class CLient Test {
+  CLient client;
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  
+}
