cbf782d28dd06e4cccf00b447ac3c75dbf890c28
Stewart ilondanga <stilondanga@gmail.com>
2017-12-15 14:22:15 +0300

    add src/test/java/StyleTest.java

--- src/test/java/StyleTest.java
+++ src/test/java/StyleTest.java
@@ -20,5 +20,10 @@ public class StylistTest {
     assertTrue(stylist instanceof Stylist);
   }
 
+  @Test
+  public void getName_returnsNameOfStylist_String() {
+    assertEquals("Tyra", stylist.getName());
+  }
+
   
 }
