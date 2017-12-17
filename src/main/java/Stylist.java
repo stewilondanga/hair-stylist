7b85eca34fde5aa9255a9c134f5459b204988841
Stewart ilondanga <stilondanga@gmail.com>
2017-12-17 23:53:19 +0300

    add src/main/java/Stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -71,5 +71,15 @@ public class Stylist {
     }
   }
 
+  public void updateName(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE stylists SET name=:name WHERE id=:id";
+      con.createQuery(sql)
+        .addParamete("name", name)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
   
 }
