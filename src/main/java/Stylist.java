0662be88c627443b329ffff012979d003d148ea0
Stewart ilondanga <stilondanga@gmail.com>
2017-12-17 23:26:16 +0300

    add src/main/java/Stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,3 +1,11 @@
 import java.util.List;
 import java.util.ArrayList;
 import org.sql2o.*;
+
+public class Stylist {
+  private String name;
+  private String description;
+  private int id;
+
+  
+}
