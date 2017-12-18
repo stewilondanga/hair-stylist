171ce2c9ec4df71f239735174a0f1524d34122f1
Stewart ilondanga <stilondanga@gmail.com>
2017-12-18 20:17:01 +0300

    add src/main/java/Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -59,5 +59,25 @@ public class Client {
     }
   }
 
+  public static Client find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients where id=:id";
+      Client client = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Client.class);
+      return client;
+    }
+  }
+
+  public void updateName(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE clients SET name=:name WHERE id=:id";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .addParameter("name", name)
+        .executeUpdate();
+    }
+  }
+
   
 }
