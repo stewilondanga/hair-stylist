a3dee59670761a1b02bd9f2e567805f016947064
Stewart ilondanga <stilondanga@gmail.com>
2017-12-18 19:54:43 +0300

    add src/main/java/Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,12 @@
 import java.util.List;
 import java.util.ArrayList;
 import org.sql2o.*;
+
+public class Client {
+  private String name;
+  private String description;
+  private int id;
+  private int stylist_id;
+
+  
+}
